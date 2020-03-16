3f0d0522b44bebdd922b276a4cc77d61ed952e48
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-03-16 16:13:13 -0400

    tags functional..working on video upload

--- backend/models/imageSchema.js
+++ backend/models/imageSchema.js
@@ -1,6 +1,4 @@
 const thingSchema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
     imageUrl: { type: String, required: true },
     recipeID: {type: String, required: true}
   })
